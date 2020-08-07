c6b58de6958763056e93feb90d6099b014bee189
Jamie DeLong <jdlong1980@gmail.com>
2020-08-07 13:33:57 -0400

    Updates

---
+++ precache-manifest.e6ff37e7e4f43c12ebe5b6e4c623a114.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe661bfd5c609dcf630cf562bcf85c3",
+    "url": "/react-app-deploy/index.html"
+  },
+  {
+    "revision": "fee6cab1919d658b829a",
+    "url": "/react-app-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db7a627cbc2b935499d5",
+    "url": "/react-app-deploy/static/js/2.a925df73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-deploy/static/js/2.a925df73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee6cab1919d658b829a",
+    "url": "/react-app-deploy/static/js/main.30a2e393.chunk.js"
+  },
+  {
+    "revision": "84383c07f8cbd1076d0d",
+    "url": "/react-app-deploy/static/js/runtime-main.f42ce0b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
